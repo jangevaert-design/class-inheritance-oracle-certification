1d0ad8dda1086a0e7448949168ac2d42d7914d22
Jan Gevaert <jan.gevaert@gmail.com>
2021-02-01 11:23:21 -0700

    Added an interface.

---
+++ src/edu/cnm/deepdive/HasTail.java
@@ -0,0 +1,9 @@
+package edu.cnm.deepdive;
+
+public interface HasTail {
+
+  int DEFAULT_TAIL_LENGTH = 2;//always public static and final but no need to type it.
+
+  int getTailLength(); //always public and abstract. Methods do not have a body.
+
+}
